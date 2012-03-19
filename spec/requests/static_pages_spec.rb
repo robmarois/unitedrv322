f125ebbb6812482b23b41e6c26440d2cbf53954f
Rob Marois <rmarois@gmail.com>
2012-03-19 07:57:15 -0400

    Refactored static pages test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,54 +4,75 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+  
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Parts"
+    page.should have_selector 'title', text: full_title('Parts')
+    click_link "Services"
+    page.should have_selector 'title', text: full_title('Services')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact Us')
+    click_link "Home"
+    page.should have_selector 'title', text: full_title('')
+  end
+  
   describe "Home page" do
   
-    before { visit root_path } 
+    before { visit root_path }
+    let(:heading)    { 'United RV' }
+    let(:page_title) { '' } 
 
-    it {should have_selector('h1', text: 'United RV') }
-    it { should have_selector('title', text: full_title('')) }
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text: '| Home' }
   end
 
   describe "Contact page" do
   
     before { visit contact_path } 
+    let(:heading)    { 'Contact Us' }
+    let(:page_title) { 'Contact' } 
 
-    it {should have_selector('h1', text: 'Contact Us')}
-    it {should have_selector('title',
-                              text: full_title('Contact Us'))}
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     
     before { visit about_path }
+    let(:heading)    { 'About Us' }
+    let(:page_title) { 'About Us' } 
 
-    it {should have_selector('h1', text: 'About Us')}
-    it {should have_selector('title',
-                              text: full_title('About Us'))}
+    it_should_behave_like "all static pages"
   end
   
   describe "Parts page" do
     before { visit parts_path }
-    
-    it {should have_selector('h1', text: 'Parts')}
-    it {should have_selector('title',
-                              text: full_title('Parts'))}
+    let(:heading)    { 'Parts' }
+    let(:page_title) { 'Parts' } 
+
+    it_should_behave_like "all static pages"
   end
   
   describe "Service page" do
     before { visit service_path }
-    
-    it {should have_selector('h1', text: 'Services')}
-    it {should have_selector('title',
-                              text: full_title('Services'))}
+    let(:heading)    { 'Services' }
+    let(:page_title) { 'Services' } 
+
+    it_should_behave_like "all static pages"
   end
   
   describe "News page" do
 
     before { visit news_path }
-    
-    it {should have_selector('h1', text: 'News')}
-    it {should have_selector('title',
-                    text: full_title('News'))}
+    let(:heading)    { 'News' }
+    let(:page_title) { 'News' } 
+
+    it_should_behave_like "all static pages"
   end
 end
