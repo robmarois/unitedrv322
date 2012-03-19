e5254c708a48f3ced48fed7d4a198713dcb6bd33
Rob Marois <rmarois@gmail.com>
2012-03-19 07:47:16 -0400

    Imported static pages from old site

--- config/application.rb
+++ config/application.rb
@@ -61,5 +61,7 @@ module Unitedrv322
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
