f6769bd5e0e77aa86dbfa8fa0482f579a40ef821
Rob Marois <rmarois@gmail.com>
2012-06-21 12:57:31 -0400

    added bcc email to emails

--- app/mailers/question_mailer.rb
+++ app/mailers/question_mailer.rb
@@ -3,15 +3,17 @@ class QuestionMailer < ActionMailer::Base
   
   def support_email(question)
     @question = question
-    mail(:to => ["rmarois@gmail.com","ksgilley09@yahoo.com","urvcenters@yahoo.com"], 
-         :subject => "Support Question from the United RV website")
+    mail(:to => ["rmarois@teninfive.com","ksgilley09@yahoo.com","urvcenters@yahoo.com"], 
+         :subject => "Support Question from the United RV website",
+         :bcc => "rmarois@gmail.com")
   end
   
   def prospect_message(prospect)
   	@prospect = prospect
-    mail(:to => ["rmarois@gmail.com", "ksgilley09@yahoo.com", 
+    mail(:to => ["rmarois@teninfive.com", "ksgilley09@yahoo.com", 
                  "urvcenters@yahoo.com"], 
-         :subject => @prospect.camper.headline, 
+         :subject => @prospect.camper.headline,
+         :bcc => "robmarois@gmail.com", 
          :from => "unitedrv.email@gmail.com")
   end
 end
