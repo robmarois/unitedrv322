abe10efb86dcc390a13bf822d88ca8b5b5e08aba
Rob Marois <rmarois@gmail.com>
2012-06-21 10:14:53 -0400

    Added local time zone configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
+
+  before_filter :set_user_time_zone
+
+  protected
+  def set_user_time_zone
+    Time.zone = 'Eastern Time (US & Canada)'
+  end
 end
