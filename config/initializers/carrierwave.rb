c3c5a36b2e2d35b372ca558de9650c2ef1dcbb02
Rob Marois <rmarois@gmail.com>
2012-06-23 15:05:10 -0400

    tweaked carrierwave.rb file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,10 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
     :aws_access_key_id      => ENV['AKIAIRGQJ37RAEJTCRQA'],
-    :aws_secret_access_key  => ENV['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']
+    :aws_secret_access_key  => ENV['acOeCSvXYv2aNHsibDKTFghrfV41NunYpfSM/t65'],
+    :region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'united_rv'
+  config.fog_public     = true                                   # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
  end
