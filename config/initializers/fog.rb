6a368519b69de353fd20850d2158e7794144ebc7
Rob Marois <rmarois@gmail.com>
2012-06-23 15:13:07 -0400

    removed carrierwave.rb file

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -9,6 +9,6 @@ CarrierWave.configure do |config|
   }
   config.fog_directory  = 'united_rv'                     # required
   # config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
-  config.fog_public     = true                                   # optional, defaults to true
+  config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
