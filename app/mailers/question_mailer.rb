73138779aac59918ecce93b7e746eb618c1d04be
Rob Marois <rmarois@gmail.com>
2012-06-23 13:34:13 -0400

    removed gmail from to: and added to bcc:

--- app/mailers/question_mailer.rb
+++ app/mailers/question_mailer.rb
@@ -3,17 +3,17 @@ class QuestionMailer < ActionMailer::Base
   
   def support_email(question)
     @question = question
-    mail(:to => ["rmarois@gmail.com","ksgilley09@yahoo.com","urvcenters@yahoo.com"], 
+    mail(:to => ["ksgilley09@yahoo.com","urvcenters@yahoo.com"], 
          :subject => "Support Question from the United RV website",
-         :bcc => "robmarois@teninfive.com")
+         :bcc => "rmarois@gmail.com")
   end
   
   def prospect_message(prospect)
   	@prospect = prospect
-    mail(:to => ["rmarois@gmail.com", "ksgilley09@yahoo.com", 
+    mail(:to => ["ksgilley09@yahoo.com", 
                  "urvcenters@yahoo.com"], 
          :subject => @prospect.camper.headline,
-         :bcc => "robmarois@teninfive.com", 
+         :bcc => "rmarois@gmail.com", 
          :from => "unitedrv.email@gmail.com")
   end
 end
