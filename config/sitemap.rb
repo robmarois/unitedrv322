d7f8bb6ecf310a4190a5939a8a1d7db335e87265
Rob Marois <rmarois@gmail.com>
2012-06-23 14:32:18 -0400

    added support for sitemaps

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,3 +1,5 @@
+# file used for Sitemap only
+
 SitemapGenerator::Sitemap.default_host = "http://www.unitedrvs.com"
 SitemapGenerator::Sitemap.sitemaps_host = "http://s3.amazonaws.com/united_rv/"
 SitemapGenerator::Sitemap.public_path = 'tmp/'
@@ -5,4 +7,13 @@ SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::WaveAdapter.new
 SitemapGenerator::Sitemap.create do
   
+  Camper.find_each do |camper|
+    add camper_path(camper), lastmod: camper.updated_at
+  end
+
+  add '/about', changefreq: 'monthly'
+  add '/service', changefreq: 'monthly'
+  add '/parts', changefreq: 'monthly'
+  add '/news', changefreq: 'monthly'
+  add '/contact', changefreq: 'monthly'  
 end
