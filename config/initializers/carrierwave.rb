8477fcf4d3eefc42ee99e2d1e1efeea07ddae857
Rob Marois <rmarois@gmail.com>
2012-06-23 14:48:57 -0400

    added carrierwave file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
-#CarrierWave.configure do |config|
-#  config.cache_dir = "#{Rails.root}/tmp/"
-#  config.storage = :fog
-  #config.permissions = 0666
-#  config.fog_credentials = {
-#    :provider               => 'AWS',
-#    :aws_access_key_id      => ENV['AKIAIRGQJ37RAEJTCRQA'],
-#    :aws_secret_access_key  => ENV['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']
-#  }
-#  config.fog_directory  = 'united_rv'
-# end
+CarrierWave.configure do |config|
+  config.cache_dir = "#{Rails.root}/tmp/"
+  config.storage = :fog
+  config.permissions = 0666
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => ENV['AKIAIRGQJ37RAEJTCRQA'],
+    :aws_secret_access_key  => ENV['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']
+  }
+  config.fog_directory  = 'united_rv'
+ end
