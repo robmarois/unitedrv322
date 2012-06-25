6b414916faae4cc6d7cde03873887ea0bd4bcc2a
Rob Marois <rmarois@gmail.com>
2012-06-24 20:12:28 -0400

    Tweaked email text, changed reply to address on emails

--- app/mailers/question_mailer.rb
+++ app/mailers/question_mailer.rb
@@ -5,12 +5,13 @@ class QuestionMailer < ActionMailer::Base
     @question = question
     mail(:to => ["ksgilley09@yahoo.com","urvcenters@yahoo.com"], 
          :subject => "Support Question from the United RV website",
-         :bcc => "rmarois@gmail.com")
+         :bcc => "rmarois@gmail.com",
+         :reply_to => @question.email)
   end
   
   def prospect_message(prospect)
   	@prospect = prospect
-    mail(:to => ["rmarois@gmail.com"], 
+    mail(:to => ["ksgilley09@yahoo.com","urvcenters@yahoo.com"], 
          :subject => @prospect.camper.headline,
          :bcc => "rmarois@gmail.com", 
          :reply_to => @prospect.email)
