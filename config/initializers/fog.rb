8361610d10d1c3a72e2700211afd6661905d2622
Rob Marois <rmarois@gmail.com>
2012-07-06 08:27:44 -0400

    tweaked fog.rb file

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -12,7 +12,7 @@ CarrierWave.configure do |config|
   }
   config.fog_directory  = primary_folder                     # required
   # config.fog_host       = 'https://assets.example.com'            # optional, defaults to nil
-  # config.fog_public     = false                                   # optional, defaults to true
+  config.fog_public     = false                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 
 end
