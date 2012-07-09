646310424c5b124c6fa3b94e56f0c8531e509448
Rob Marois <rmarois@gmail.com>
2012-07-09 11:08:37 -0400

    Added formatted 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,20 +4,21 @@ class ApplicationController < ActionController::Base
 
   before_filter :set_user_time_zone
 
-  def render_optional_error_file(status_code)
-    if status_code == :not_found
-      render_404
-    else
-      super
-    end
+    rescue_from ActiveRecord::RecordNotFound, :with => :render_record_not_found
+
+  # Catch record not found for Active Record
+  def render_record_not_found
+    render :template => "shared/catchmissingpage", :layout => false, :status => 404
   end
 
-  def render_404
-    respond_to do |type| 
-      type.html { render :template => "errors/error_404", :layout => 'application', :status => 404 } 
-      type.all  { render :nothing => true, :status => 404 } 
-    end
-    true  # so we can do "render_404 and return"
+  # Catches any missing methods and calls the general render_missing_page method
+  def method_missing(*args)
+    render_missing_page # calls my common 404 rendering method
+  end
+
+  # General method to render a 404
+  def render_missing_page
+    render :template => "shared/catchmissingpage", :layout => false, :status => 404
   end
 
 
