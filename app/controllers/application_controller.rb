22b215d7061afce6446e55ddf81b5d0809a77fe3
Rob Marois <rmarois@gmail.com>
2012-07-09 11:05:29 -0400

    Added formatted 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,23 @@ class ApplicationController < ActionController::Base
 
   before_filter :set_user_time_zone
 
+  def render_optional_error_file(status_code)
+    if status_code == :not_found
+      render_404
+    else
+      super
+    end
+  end
+
+  def render_404
+    respond_to do |type| 
+      type.html { render :template => "errors/error_404", :layout => 'application', :status => 404 } 
+      type.all  { render :nothing => true, :status => 404 } 
+    end
+    true  # so we can do "render_404 and return"
+  end
+
+
   protected
   def set_user_time_zone
     Time.zone = 'Eastern Time (US & Canada)'
